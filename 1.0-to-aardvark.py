9c89d8fe348a66437ae09115c99f96d9e72fb61a
Stephen Appel <srappel930@gmail.com>
2024-03-14 10:25:06 -0500

    add local functions and other localizations

--- 1.0-to-aardvark.py
+++ 1.0-to-aardvark.py
@@ -6,14 +6,19 @@ from datetime import datetime
 from pathlib import Path
 
 # Manual changes before run
+
+# Path to crosswalk definition
 dir_crosswalk = Path('crosswalk.csv')
 # add directory of JSON files in the 1.0 schema
 dir_old_schema = Path('1.0/')
 #add directory for new JSON files in the Aardvark schema
 dir_new_schema = Path('aardvark/')
 
-# Load the crosswalk.csv and make it a dictionary
+#Default values
+RESOURCE_CLASS_DEFAULT = "Datasets"
+PLACE_DEFAULT = "Wisconsin"
 
+# Load the crosswalk.csv and make it a dictionary
 crosswalk = {}
 with open(dir_crosswalk, encoding='utf8') as f:
     reader = csv.reader(f)
@@ -23,6 +28,66 @@ with open(dir_crosswalk, encoding='utf8') as f:
         new = record[1]
         crosswalk[old] = new
 
+# Function to convert fields that ends with '_sm' to an array
+# SRA - Moved this to before the other function because it calls this function
+def string2array(data_dict):
+    for key in data_dict.keys():
+        suffix = key.split('_')[-1]
+        if suffix == 'sm' or suffix == 'im':
+            val = data_dict[key]
+            if type(val) != list:
+                data_dict[key] = [val]
+    return data_dict
+
+def check_required(data_dict):
+    # Check if required fields are present
+    requirements = ["dct_publisher_sm", "dct_spatial_sm", "gbl_mdVersion_s", "dct_title_s", "gbl_resourceClass_sm", "id", "gbl_mdModified_dt"]
+    
+    for req in requirements:
+        if req not in data_dict:
+            print (f"Requirement {req} is not present...")
+            
+        if req == "gbl_resourceClass_sm":
+            if "dc_type_s" not in data_dict:
+                print(f"No dc_type_s information found.")
+                data_dict["gbl_resourceClass_sm"] = [RESOURCE_CLASS_DEFAULT]
+            elif data_dict["dc_type_s"] == "Dataset":
+                data_dict["gbl_resourceClass_sm"] = ["Datasets"]
+                print(f"Replaced dc_type_s:Dataset with gbl_resourceClass_sm:Datasets")
+            elif data_dict["dc_type_s"] == "Image":
+                data_dict["gbl_resourceClass_sm"] = ["Imagery"]
+                print(f"Replaced dc_type_s:Dataset with gbl_resourceClass_sm:Imagery")
+            else:
+                data_dict["gbl_resourceClass_sm"] = [RESOURCE_CLASS_DEFAULT] # change if needed!
+                print(f"Replaced dc_type_s:Null with gbl_resourceClass_sm:{RESOURCE_CLASS_DEFAULT}")
+                
+        elif req == "dct_spatial_sm":
+            data_dict["dct_spatial_sm"] = [PLACE_DEFAULT]
+            print(f"Replaced dct_spatial_sm:Null with dct_spatial_sm:{PLACE_DEFAULT}")
+            
+        elif req == "gbl_mdModified_dt":
+            data_dict["gbl_mdModified_dt"] = datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")
+            
+        elif req == "dct_publisher_sm":
+            print(f"No Publisher information found.")
+            if "dct_creator_sm" not in data_dict:
+                print(f"No Creator information found.")
+            elif data_dict["dct_creator_sm"] != "":
+                data_dict["dct_publisher_sm"] = data_dict["dct_creator_sm"]
+                print(f"Replaced dct_publisher_sm:Null with dct_publisher_sm:{data_dict['dct_creator_sm']}")
+            else:
+                continue
+    return
+
+def remove_depricated(data_dict):
+    # Remove the depricated fields from the output Aardvark
+    depricated = ["dc_type_s", "layer_geom_type_s", "dct_isPartOf_sm", "uw_supplemental_s", "uw_notice_s"]
+    for field in depricated:
+        if field in data_dict:
+            data_dict.pop(field)
+            print(f"Removed the depricated {field} field.")
+    
+    return
 
 # Function to update the metadata schema
 def schema_update(filepath):
@@ -35,32 +100,24 @@ def schema_update(filepath):
         for old_schema, new_schema in crosswalk.items():
             if old_schema in data:
                 data[new_schema] = data.pop(old_schema)
-
+        
         # Change the metadata type:
         data["gbl_mdVersion_s"] = "Aardvark"
-
-        # Remove geoblacklight_version
-        if "geoblacklight_version" in data:
-            data.pop("geoblacklight_version")
-            
+        
+        # Check for required fields:
+        check_required(data)
+        
+        # Remove depricated fields
+        remove_depricated(data)
+        
     # check for multi-valued fields - if so, convert its value to an array
     data = string2array(data)
 
     # Write updated JSON to a new folder
     filepath_updated = dir_new_schema / file
-    with open(filepath_updated, 'w', encoding='utf8') as fw:
+    with open(filepath_updated, 'w') as fw:
         j = json.dumps(data, indent=2)
         fw.write(j)
-
-# Function to convert fields that ends with '_sm' to an array
-def string2array(dict):
-    for key in dict.keys():
-        suffix = key.split('_')[-1]
-        if suffix == 'sm' or suffix == 'im':
-            val = dict[key]
-            if type(val) != list:
-                dict[key] = [val]
-    return dict
 
 
 # Collect all JSON files in a list
@@ -70,4 +127,3 @@ for file in files:
     print(f'Executing {file} ...')
     filepath = dir_old_schema / file
     schema_update(filepath)
-
